505dcc25a3c2e2d25d08616e4251aeccf4397be2
Valentine Okosun <ejodamen.okosun@andela.com>
2019-08-23 13:03:12 +0100

    implement crud for degree

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import usersRoute from './users';
 import advisorsRoute from './advisors';
+import degreesRoute from './degrees';
 import { Response } from '../helpers/utils';
 import { STATUS, MESSAGE } from '../helpers/constants';
 
@@ -9,6 +10,8 @@ const router = express.Router();
 router.use('/users', usersRoute);
 
 router.use('/advisors', advisorsRoute);
+
+router.use('/degrees', degreesRoute);
 
 router.all('*', (request, response) => {
   Response.send(response, STATUS.NOT_FOUND, null, MESSAGE.NOT_FOUND, false);
