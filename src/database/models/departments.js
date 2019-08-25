ebbaacc189694647e8e805f01ab3bd65da2624fc
Valentine Okosun <ejodamen.okosun@andela.com>
2019-08-25 09:26:42 +0100

    feat(models): refactor model schema

--- src/database/models/departments.js
+++ src/database/models/departments.js
@@ -1,48 +1,7 @@
-export default (sequelize, DataTypes) => {
-  const Department = sequelize.define('Department', {
-    id: {
-      allowNull: false,
-      primaryKey: true,
-      type: DataTypes.INTEGER,
-    },
-    name: {
-      type: DataTypes.STRING,
-      unique: true,
-    },
-    createdAt: {
-      type: DataTypes.DATE,
-      defaultValue: sequelize.NOW,
-    },
-    updatedAt: {
-      type: DataTypes.DATE,
-      defaultValue: sequelize.NOW,
-      onUpdate: sequelize.NOW,
-    },
-    collegeId: {
-      type: DataTypes.INTEGER,
-    },
-  });
+import { commonModel } from './template';
 
-  /**
-   * Get a department if exist
-   * @param {string} column Column to check against
-   * @param {string} value Value to lookup
-   * @returns {object} The details if found, null
-   */
-  Department.getDepartment = async (column, value) => {
-    let result = null;
-    try {
-      const { dataValues } = await Department.findOne({
-        where: {
-          [column]: value,
-        },
-      });
-      result = dataValues;
-    } catch (error) {
-      // TODO
-    }
-    return result;
-  };
+export default (sequelize, DataTypes) => {
+  const Department = sequelize.define('Department', commonModel(DataTypes, sequelize));
 
   Department.associate = (models) => {
     Department.belongsTo(models.Faculty, {
