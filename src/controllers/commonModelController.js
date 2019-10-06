3efde32d015be75f5efb9813a4e3fe778913c2e1
Valentine Okosun <ejodamen.okosun@andela.com>
2019-10-06 17:44:15 +0100

    Update commonModelController.js

--- src/controllers/commonModelController.js
+++ src/controllers/commonModelController.js
@@ -43,7 +43,7 @@ export class CommonModelController {
         response,
         STATUS.SERVER_ERROR,
         error,
-        'Server error, please try agains.',
+        'Server error, please try again.',
         false,
       );
     }
