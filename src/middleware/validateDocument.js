035d9cc7448b9b678f3e1bceb550d8db301cab97
Valentine Okosun <ejodamen.okosun@andela.com>
2019-10-10 11:04:57 +0100

    fix minor bug

--- src/middleware/validateDocument.js
+++ src/middleware/validateDocument.js
@@ -5,22 +5,15 @@ import { Response } from '../helpers/utils';
 import { STATUS } from '../helpers/constants';
 
 const {
-  Degree,
-  Faculty,
   Department,
   Sequelize: { Op },
 } = models;
 
-const mapBooleanToArray = (degree, faculty, department) => {
+const mapBooleanToArray = (department) => {
   const array = [];
-  if (!degree) {
-    array.push({ degreeId: 'The choosen degree is unkown' });
-  }
-  if (!faculty) {
-    array.push({ facultyId: 'The choosen faculty is unkown' });
-  }
+
   if (!department) {
-    array.push({ departmentId: 'The choosen department is unkown within the choosen faculty' });
+    array.push({ departmentId: 'The choosen department is unkown' });
   }
   return array;
 };
@@ -36,27 +29,19 @@ export const validateCreateDocument = [
   validateRequired('title'),
   validateRequired('author'),
   validateUrl('url', 'Document upload link must be a valid URL'),
-  validateNumber('degreeId', 'Degree ID must be 7 digit number'),
   validateNumber('type', 'The type of document must be either 1 or 0', 0, 1),
-  validateNumber('facultyId', 'Faculty ID must be 7 digit number'),
   validateNumber('departmentId', 'Department ID must be 7 digit number'),
 ];
 
 export const validateRelationships = async (request, response, next) => {
-  const { degreeId, facultyId, departmentId } = request.body;
+  const { departmentId } = request.body;
   const { collegeId } = response.locals;
   try {
-    const degree = await Degree.findOne({
-      where: { [Op.and]: { collegeId, id: degreeId } },
-    });
-    const faculty = await Faculty.findOne({
-      where: { [Op.and]: { collegeId, id: facultyId } },
-    });
     const department = await Department.findOne({
-      where: { [Op.and]: { collegeId, id: departmentId, facultyId } },
+      where: { [Op.and]: { collegeId, id: departmentId } },
     });
 
-    const message = mapBooleanToArray(degree, faculty, department);
+    const message = mapBooleanToArray(department);
 
     if (message.length) {
       return Response.send(response, STATUS.NOT_FOUND, message, '', false);
